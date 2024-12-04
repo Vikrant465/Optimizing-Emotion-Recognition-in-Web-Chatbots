38612b4d064a1fa66c196adeb38022373f11c7b7
vikrant <viksingh465@gmail.com>
2024-12-04 23:37:28 +0530

    nav bar added

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+
+
+
+export default function Home1() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1>hello there, write your code here SHIVAM SINGH </h1>
+      </div>
+    );
+  }
+  
