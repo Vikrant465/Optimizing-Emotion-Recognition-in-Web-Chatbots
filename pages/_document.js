57d87139ab6d5d65a5229307270668caf7c710f3
Atul-8383 <atulprajapatipop@gmail.com>
2024-12-08 14:53:27 +0530

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Ensure ResponsiveVoice Script is added correctly */}
+        <script
+          src="https://code.responsivevoice.org/responsivevoice.js?key=kAGcZKWy"
+          async
+        ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
