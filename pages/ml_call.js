b20fa3bd43b3a2adee2476cb225c3a226c7d35ca
Vikrant465 <viksingh465@gmail.com>
2024-12-21 22:37:02 +0530

    commit

--- pages/ml_call.js
+++ pages/ml_call.js
@@ -1,43 +1,43 @@
-// import axios from "axios";
-// import { useState } from "react";
+import axios from "axios";
+import { useState } from "react";
 
-// const Home = () => {
-//   const [userInput, setUserInput] = useState("");
-//   const [response, setResponse] = useState(null);
+const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [response, setResponse] = useState(null);
 
-//   const handleSubmit = async () => {
-//     try {
-//       const res = await axios.post("http://127.0.0.1:8000/process", {
-//         question: userInput,
-//       });
-//       setResponse(res.data);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("http://127.0.0.1:8000/process", {
+        question: userInput,
+      });
+      setResponse(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-//   return (
-//     <div className="home bg-a flex items-center h-screen justify-around">
-//         <div className="">
-//         <input
-//             type="text"
-//             value={userInput}
-//             onChange={(e) => setUserInput(e.target.value)}
-//         />
-//         <button color="primary" onClick={handleSubmit}>
-//             Submit
-//         </button>
-//         {response && (
-//             <div>
-//             <p>AI Response: {response.ai_response}</p>
-//             <p>Emotion: {response.predicted_emotion}</p>
-//             </div>
-//         )}
-//         </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="home bg-a flex items-center h-screen justify-around">
+        <div className="">
+        <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+        />
+        <button color="primary" onClick={handleSubmit}>
+            Submit
+        </button>
+        {response && (
+            <div>
+            <p>AI Response: {response.ai_response}</p>
+            <p>Emotion: {response.predicted_emotion}</p>
+            </div>
+        )}
+        </div>
+    </div>
+  );
+};
 
-// export default Home;
+export default Home;
 
 
